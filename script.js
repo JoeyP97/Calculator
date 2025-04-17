c9d56339c3216caad8f5b031cf7a5ad7242f8f00
Joseph <168624685+JoeyP97@users.noreply.github.com>
2025-04-17 15:43:40 +1000

    add functionality to calculator

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const clear = document.querySelector("#clear")
 const calc = document.querySelectorAll(".calc")
 const equals = document.querySelector("#equals")
 
-let first 
-let second 
-let operator 
+let first = 0
+let second  = 0
+let operator = null
 
 // if operater == null and input is numeric make first
 // if operator == !null amd input is numeric make second
 // if operator == null and input is !numeric and not . make operator
+
 
 const text = document.createElement("div")
 
@@ -22,12 +23,23 @@ function addEventByClass(className, event, fn) {
 
 
 function addToScreen(e) {
-    if (operator == null && typeof Number(e.target.id) === 'number') {
-        let first = e.target.id
-        text.textContent += `${first}`
-        screens.append(text) 
+    let theText = text.textContent
+    if (theText.toString().length < 8) {
+        if (operator == null && isNaN(e.target.id) == false || operator == null && e.target.id == '.') {
+            first += e.target.id
+            text.textContent += `${e.target.id}`
+            screens.append(text) 
+        } else if (operator != null && isNaN(e.target.id) == false || operator != null && e.target.id == '.') {
+            second += e.target.id
+            text.textContent += `${e.target.id}`
+            screens.append(text)
+        } else if (operator == null && isNaN(e.target.id) == true && e.target.id != '.') {
+            operator = e.target.id
+            text.textContent += `${operator}`
+            screens.append(text)
+        }
     }
-    
+    theText = text.textContent
 }
 
 addEventByClass("calc", "click", addToScreen)
@@ -41,9 +53,55 @@ clear.addEventListener("click", () => {
 })
 
 function equate() {
-    let answer = first * second
-    text.textContent = `${answer}`
-    screens.append(text)
+    let answer
+    if (operator == '+'){
+          answer = +first + +second
+    
+        if (answer.toString().length <= 8) {
+          text.textContent = `${answer}`
+          screens.append(text)
+        }
+        else {
+          text.textContent = 'Error'
+          screens.append(text)
+         }
+    } else if (operator == '-'){
+          answer = +first - +second
+    
+        if (Number(first <= 99999999)) {
+          text.textContent = `${answer}`
+          screens.append(text)
+        }
+        else {
+          text.textContent = 'Error'
+          screens.append(text)
+         }
+    } else if (operator =='x'){
+          answer = +first * +second
+    
+        if (Number(first <= 99999999)) {
+          text.textContent = `${answer}`
+          screens.append(text)
+        }
+        else {
+          text.textContent = 'Error'
+          screens.append(text)
+         }
+    } else if (operator == '/'){
+          answer = +first / +second
+    
+        if (Number(first <= 99999999)) {
+          text.textContent = `${answer}`
+          screens.append(text)
+        }
+        else {
+          text.textContent = 'Error'
+          screens.append(text)
+         }
+    }
+    operator = null
+    first = answer
+    second = 0
 }
 
 equals.addEventListener("click", () => {
